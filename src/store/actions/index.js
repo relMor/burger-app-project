284d005fe1b6bb82bcd02749ecf78818fff93e73
realMortred <amanvarma.600847@gmail.com>
2020-08-02 21:56:55 +0530

    Made action creator lean with redux saga

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,9 +2,21 @@ export {
   addIngredient,
   removeIngredient,
   initIngredients,
+  setIngredients,
+  fetchIngredientsFailed,
 } from "./burgerBuilder";
 
-export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
+export {
+  purchaseBurger,
+  purchaseInit,
+  fetchOrders,
+  purchaseBurgerStart,
+  purchaseBurgerFail,
+  purchaseBurgerSuccess,
+  fetchOrderStart,
+  fetchOrdersFail,
+  fetchOrdersSuccess,
+} from "./order";
 
 export {
   auth,
@@ -12,4 +24,8 @@ export {
   setAuthRedirectPath,
   authCheckState,
   logoutSucceed,
+  authStart,
+  authSuccess,
+  authFail,
+  checkAuthTimeout,
 } from "./auth";
